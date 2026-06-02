89312466e8836da1583ce3f1607792e19024bbb4
Evan <evan.c.kelch@gmail.com>
2026-06-01 23:20:27 -0500

    added a twee

--- scripts/load_screen.gd
+++ scripts/load_screen.gd
@@ -1,14 +1,14 @@
 extends CanvasLayer
 
 signal loading_screen_ready
+@onready var progress_bar: TextureProgressBar = $TextureProgressBar
 
 func _ready() -> void:
 	#can animate start and await finish
-	print("going to emit")
 	loading_screen_ready.emit()
 
 func _on_progress_changed(new_value: float) -> void:
-	pass
+	progress_bar.value = new_value
 
 func _on_load_finished() -> void:
 	#backward animation await
